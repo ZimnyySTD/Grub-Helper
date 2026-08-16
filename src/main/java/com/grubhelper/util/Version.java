3e5d7a1d834104e61e8de7247e47c307556eeb88
Constantine Peter Brox <cobra002@osloskolen.no>
2026-08-16 17:54:51 +0200

    Update Version.java

--- src/main/java/com/grubhelper/util/Version.java
+++ src/main/java/com/grubhelper/util/Version.java
@@ -10,7 +10,7 @@ package com.grubhelper.util;
  */
 public class Version {
     // Current release version string adhering to vX.Y.Z semantic versioning
-    public static final String CURRENT_VERSION = "1.0.3";
+    public static final String CURRENT_VERSION = "1.1.3";
 
     // Official GitHub repository clone URL
     public static final String REPO_URL = "https://github.com/ZimnyySTD/Grub-Helper.git";
